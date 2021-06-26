adc1c3c656a75748d629652770f671690439c532
Henry HENG Hian Wee <deezombiedude612@hotmail.com>
2021-06-27 05:07:54 +0800

    Update gcc-instructions.sh

--- gcc-instructions.sh
+++ gcc-instructions.sh
@@ -3,12 +3,16 @@
 brew install gcc
 brew install g++
 
-# Head over to /usr/local/bin. 
-cd /usr/local/bin
-ls
+# Check if gcc or g++ exists inside /usr/local/bin/
+ls /usr/local/bin | grep gcc
+ls /usr/local/bin | grep g++
 
-# Find gcc-<version_num_here>. From here, do the following (version 10 as of making this guide):
-ln -s g++-10 g++
-ln -s gcc-10 gcc
+# Make backups if found
+cp -R /usr/local/bin/gcc /usr/local/bin/gcc.bak
+cp -R /usr/local/bin/g++ /usr/local/bin/g++.bak
 
-# Restart your Mac.
+# Find gcc-<version_num_here>. From here, do the following (version 11 as of making this guide):
+sudo ln -Fs $(which gcc-11) /usr/local/bin/gcc
+sudo ln -Fs $(which g++-11) /usr/local/bin/g++
+
+# It should work in a new Terminal window now.
