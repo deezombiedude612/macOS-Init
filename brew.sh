fcde2f8d4fb8b7f19eaa8b055297829836934b02
Henry Heng <deezombiedude612@hotmail.com>
2025-07-17 04:15:38 +0800

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -18,19 +18,23 @@ brew install pyenv
 brew install tree
 brew install webp
 
-brew install --cask 4k-video-downloader
+# brew install --cask 4k-video-downloader
+brew install --cask 4k-video-downloader+
 brew install --cask 4k-video-to-mp3
 brew install --cask 4k-youtube-to-mp3
 brew install --cask android-studio
 brew install --cask audacity
 brew install --cask burp-suite
 brew install --cask coconutbattery
+brew install --cask dbngin
 brew install --cask discord
+brew install --cask docker-desktop
 brew install --cask firefox
 brew install --cask github
-brew install --cask google-chrome
+# brew install --cask google-chrome
 brew install --cask herd
 brew install --cask intellij-idea-ce
+brew install --cask inkscape
 brew install --cask iterm2
 brew install --cask mactex
 brew install --cask mendeley-reference-manager
@@ -43,13 +47,17 @@ brew install --cask opera
 brew install --cask postman
 brew install --cask pycharm-ce
 brew install --cask rstudio
-brew install --cask skype
+# brew install --cask skype
+brew install --cask stats
 brew install --cask sublime-text
+brew install --cask tableplus
 brew install --cask tuxera-ntfs
 brew install --cask visual-paradigm-ce
 brew install --cask visual-studio-code
+brew install --cask vivaldi
 brew install --cask vlc
-brew install --cask vmware-fusion
+# brew install --cask vmware-fusion (disabled)
 brew install --cask warp
+# brew install --cask whatsapp
 brew install --cask wireshark
 brew install --cask zoom
